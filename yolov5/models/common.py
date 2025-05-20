54e5aa3c800247d37a30b73f951d216aeb4e0fda
Isleyter <151705437+Isleyter@users.noreply.github.com>
2025-05-20 14:10:19 -0500

    Update common.py

--- yolov5/models/common.py
+++ yolov5/models/common.py
@@ -36,7 +36,7 @@ except (ImportError, AssertionError):
 
 from ultralytics.utils.plotting import Annotator, colors, save_one_box
 
-from ..utils import TryExcept
+from yolov5.utils import TryExcept
 from utils.dataloaders import exif_transpose, letterbox
 from utils.general import (
     LOGGER,
