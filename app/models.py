bdea47554a08597a282593dfeb0af08bd1d3f0e4
Isleyter <151705437+Isleyter@users.noreply.github.com>
2025-05-20 21:48:48 -0500

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,8 +1,10 @@
 from flask_login import UserMixin
-from flask import current_app
-from app.extensions import bcrypt
+from app.extensions import mongo, bcrypt
 from bson.objectid import ObjectId
-from datetime import datetime
+
+# Definir las colecciones globales para evitar repetir mongo.db
+users_collection = mongo.db.users
+monitoreos_collection = mongo.db.monitoreos
 
 # -------------------- MODELO USUARIO --------------------
 class User(UserMixin):
@@ -13,40 +15,35 @@ class User(UserMixin):
 
     @staticmethod
     def get_by_id(user_id):
-        mongo = current_app.extensions["pymongo"]
-        data = mongo.db.users.find_one({"_id": ObjectId(user_id)})
+        data = users_collection.find_one({"_id": ObjectId(user_id)})
         return User(data) if data else None
 
     @staticmethod
     def get_by_email(email):
-        mongo = current_app.extensions["pymongo"]
-        data = mongo.db.users.find_one({"email": email})
+        data = users_collection.find_one({"email": email})
         return User(data) if data else None
 
     def check_password(self, password):
         return bcrypt.check_password_hash(self.password, password)
 
+
 # -------------------- MODELO MONITOREO --------------------
 class Monitoreo:
-
     @staticmethod
     def guardar(data):
-        mongo = current_app.extensions["pymongo"]
-        return mongo.db.monitoreos.insert_one(data)
+        return monitoreos_collection.insert_one(data)
 
     @staticmethod
     def obtener_todos(filtro=None, orden=None):
-        mongo = current_app.extensions["pymongo"]
         query = filtro or {}
         sort = orden or [("fecha", -1)]
-        return list(mongo.db.monitoreos.find(query).sort(sort))
+        return list(monitoreos_collection.find(query).sort(sort))
 
     @staticmethod
     def obtener_paginado(filtro=None, pagina=1, por_pagina=5):
-        mongo = current_app.extensions["pymongo"]
         filtro = filtro or {}
         saltar = (pagina - 1) * por_pagina
-        resultados = mongo.db.monitoreos.find(filtro).sort("fecha", -1).skip(saltar).limit(por_pagina)
+        resultados = monitoreos_collection.find(filtro).sort("fecha", -1).skip(saltar).limit(por_pagina)
 
         resultados_list = list(resultados)
         for m in resultados_list:
@@ -56,10 +53,8 @@ class Monitoreo:
 
     @staticmethod
     def eliminar_por_id(monitoreo_id):
-        mongo = current_app.extensions["pymongo"]
-        return mongo.db.monitoreos.delete_one({"_id": ObjectId(monitoreo_id)})
+        return monitoreos_collection.delete_one({"_id": ObjectId(monitoreo_id)})
 
     @staticmethod
     def obtener_por_id(monitoreo_id):
-        mongo = current_app.extensions["pymongo"]
-        return mongo.db.monitoreos.find_one({"_id": ObjectId(monitoreo_id)})
+        return monitoreos_collection.find_one({"_id": ObjectId(monitoreo_id)})
