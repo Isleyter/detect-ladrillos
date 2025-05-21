f7b3259ff97ab9c3beffa7c200b08690adc5fe55
Isleyter <151705437+Isleyter@users.noreply.github.com>
2025-05-20 21:27:18 -0500

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from flask_login import UserMixin
-from app.extensions import mongo, bcrypt
+from flask import current_app
+from app.extensions import bcrypt
 from bson.objectid import ObjectId
 from datetime import datetime
 
@@ -12,11 +13,13 @@ class User(UserMixin):
 
     @staticmethod
     def get_by_id(user_id):
+        mongo = current_app.extensions["pymongo"]
         data = mongo.db.users.find_one({"_id": ObjectId(user_id)})
         return User(data) if data else None
 
     @staticmethod
     def get_by_email(email):
+        mongo = current_app.extensions["pymongo"]
         data = mongo.db.users.find_one({"email": email})
         return User(data) if data else None
 
@@ -25,20 +28,22 @@ class User(UserMixin):
 
 # -------------------- MODELO MONITOREO --------------------
 class Monitoreo:
-    
+
     @staticmethod
     def guardar(data):
+        mongo = current_app.extensions["pymongo"]
         return mongo.db.monitoreos.insert_one(data)
-    
 
     @staticmethod
     def obtener_todos(filtro=None, orden=None):
+        mongo = current_app.extensions["pymongo"]
         query = filtro or {}
         sort = orden or [("fecha", -1)]
         return list(mongo.db.monitoreos.find(query).sort(sort))
-    
+
     @staticmethod
     def obtener_paginado(filtro=None, pagina=1, por_pagina=5):
+        mongo = current_app.extensions["pymongo"]
         filtro = filtro or {}
         saltar = (pagina - 1) * por_pagina
         resultados = mongo.db.monitoreos.find(filtro).sort("fecha", -1).skip(saltar).limit(por_pagina)
@@ -49,15 +54,12 @@ class Monitoreo:
 
         return resultados_list
 
-    
-
-
     @staticmethod
     def eliminar_por_id(monitoreo_id):
-        from bson.objectid import ObjectId
+        mongo = current_app.extensions["pymongo"]
         return mongo.db.monitoreos.delete_one({"_id": ObjectId(monitoreo_id)})
 
     @staticmethod
     def obtener_por_id(monitoreo_id):
-        from bson.objectid import ObjectId
+        mongo = current_app.extensions["pymongo"]
         return mongo.db.monitoreos.find_one({"_id": ObjectId(monitoreo_id)})
