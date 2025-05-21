373d0a92b87731f19d84f6800d9faaef9da188b6
Isleyter <151705437+Isleyter@users.noreply.github.com>
2025-05-20 22:00:35 -0500

    Update extensions.py

--- app/extensions.py
+++ app/extensions.py
@@ -1,7 +1,7 @@
 from flask_login import LoginManager
-from flask_bcrypt import Bcrypt # type: ignore
-from flask_migrate import Migrate # type: ignore
-from flask_pymongo import PyMongo # type: ignore
+from flask_bcrypt import Bcrypt  # type: ignore
+from flask_migrate import Migrate  # type: ignore
+from flask_pymongo import PyMongo  # type: ignore
 
 login_manager = LoginManager()
 bcrypt = Bcrypt()
